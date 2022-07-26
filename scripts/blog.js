3c18b2968146e58c0dc51c4d16cba3cc9f26e7a7
joeikedo <jikedo@ucsd.edu>
2022-07-26 02:12:05 -0700

    working without localstorage

--- scripts/blog.js
+++ scripts/blog.js
@@ -7,7 +7,7 @@ let postListTag;
 function injectListItems(){
     let postAggregate = '';
     for(let i = 0; i < postInfoArray.length; i++){
-        postAggregate = postAggregate + `<li id="${postInfoArray[i].parentId}">${postInfoArray[i].parentId}: ${postInfoArray[i].summary} <button data-parent-id-edit="${postInfoArray[i].parentId}" data-parent-summary="${postInfoArray[i].summary}" onclick="editPost(this.getAttribute('data-parent-id-edit'), this.getAttribute('data-parent-summary'))">Edit</button><button data-parent-id="${postInfoArray[i].parentId}" onclick="deletePost(this.getAttribute('data-parent-id'))">Delete</button></li>`
+        postAggregate = postAggregate + `<li id="${postInfoArray[i].parentId}">${postInfoArray[i].title} - (${postInfoArray[i].date}): ${postInfoArray[i].summary} <button data-parent-id-edit="${postInfoArray[i].parentId}" data-parent-summary="${postInfoArray[i].summary}" data-parent-title="${postInfoArray[i].title}" data-parent-date="${postInfoArray[i].date}" onclick="editPost(this.getAttribute('data-parent-id-edit'), this.getAttribute('data-parent-summary'), this.getAttribute('data-parent-title'), this.getAttribute('data-parent-date'))">Edit</button><button data-parent-id="${postInfoArray[i].parentId}" onclick="deletePost(this.getAttribute('data-parent-id'))">Delete</button></li>`
     }
     postListTag.innerHTML = postAggregate;
 } 
@@ -15,9 +15,13 @@ function injectListItems(){
 
 //Add Post
 let addPostDialog;
+let addTitleInput;
 let addPostInput;
+let addDateInput;
 function addPostFunction(event){
     addPostInput.value = '';
+    addDateInput.value = '';
+    addTitleInput.value = '';
     addPostDialog.showModal();
 }
 
@@ -30,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () =>
 
     //Add Post
     addPostInput = document.getElementById('postInput');
+    addDateInput = document.getElementById('addDate');
+    addTitleInput = document.getElementById('addTitle');
     const addPostButton = document.getElementById('addPostButton');
     addPostButton.addEventListener('click', addPostFunction);
 
@@ -43,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () =>
             else if(addPostDialog.returnValue == 'ok'){
                 //Add the new post to local storage so it can be retrieved on subsequent page loads.
                 const postId = crypto.randomUUID();
-                const newPost = {parentId: postId, summary: addPostInput.value}
+                const newPost = {parentId: postId, summary: addPostInput.value, date: addDateInput.value, title: addTitleInput.value}
                 postInfoArray.push(newPost);
 
                 //Add the actual post info to the markup of the page
@@ -69,9 +75,15 @@ document.addEventListener('DOMContentLoaded', () =>
         {
             if(editPostDialog.returnValue == 'ok'){
                 const editInputValue = document.getElementById('editPostInput').value;
+                const editTitleValue = document.getElementById('editTitle').value;
+                const editDateValue = document.getElementById('editDate').value;
+
                 const indexOfObject = postInfoArray.findIndex((obj => obj.parentId == window.currentPostId));
                 
                 postInfoArray[indexOfObject].summary = editInputValue;
+                postInfoArray[indexOfObject].title = editTitleValue;
+                postInfoArray[indexOfObject].date = editDateValue;
+                
                 injectListItems();
                 window.currentPostId = '';
             }
