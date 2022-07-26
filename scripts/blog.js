a0969cdc256eae7e76dcd4490501f0effdb13e3c
joeikedo <jikedo@ucsd.edu>
2022-07-25 20:39:58 -0700

    all basics working

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,12 +1,13 @@
+//Used to pass current post id being deleted/edited
+window.currentPostId = ''; 
 //The array that stores the Post info. Only for testing, will replace with local storage.
-window.currentPostId = ''; //Used to pass current post id being deleted/edited
 let postInfoArray = [];
-let postListTag;
 
+let postListTag;
 function injectListItems(){
     let postAggregate = '';
     for(let i = 0; i < postInfoArray.length; i++){
-        postAggregate = postAggregate + `<li id="${postInfoArray[i].parentId}">${postInfoArray[i].parentId}: ${postInfoArray[i].summary} <button data-parent-id-edit="${postInfoArray[i].parentId}" onclick="editPost(this.getAttribute('data-parent-id-edit'))">Edit</button><button data-parent-id="${postInfoArray[i].parentId}" onclick="deletePost(this.getAttribute('data-parent-id'))">Delete</button></li>`
+        postAggregate = postAggregate + `<li id="${postInfoArray[i].parentId}">${postInfoArray[i].parentId}: ${postInfoArray[i].summary} <button data-parent-id-edit="${postInfoArray[i].parentId}" data-parent-summary="${postInfoArray[i].summary}" onclick="editPost(this.getAttribute('data-parent-id-edit'), this.getAttribute('data-parent-summary'))">Edit</button><button data-parent-id="${postInfoArray[i].parentId}" onclick="deletePost(this.getAttribute('data-parent-id'))">Delete</button></li>`
     }
     postListTag.innerHTML = postAggregate;
 } 
@@ -42,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () =>
             else if(addPostDialog.returnValue == 'ok'){
                 //Add the new post to local storage so it can be retrieved on subsequent page loads.
                 const postId = crypto.randomUUID();
-                const newPost = {parentId: postId, summary: postInput.value}
+                const newPost = {parentId: postId, summary: addPostInput.value}
                 postInfoArray.push(newPost);
 
                 //Add the actual post info to the markup of the page
@@ -57,6 +58,20 @@ document.addEventListener('DOMContentLoaded', () =>
             if(deletePostDialog.returnValue == 'ok'){
                 const filteredArray = postInfoArray.filter(function(e) { return e.parentId !== window.currentPostId });
                 postInfoArray = filteredArray;
+                injectListItems();
+                window.currentPostId = '';
+            }
+        }
+    )
+
+    const editPostDialog = document.getElementById('editPostDialog');
+    editPostDialog.addEventListener('close', ()=>  
+        {
+            if(editPostDialog.returnValue == 'ok'){
+                const editInputValue = document.getElementById('editPostInput').value;
+                const indexOfObject = postInfoArray.findIndex((obj => obj.parentId == window.currentPostId));
+                
+                postInfoArray[indexOfObject].summary = editInputValue;
                 injectListItems();
                 window.currentPostId = '';
             }
